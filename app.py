24a912ef12b9d7def687c0d4d46e8d49d729269f
Ashwani Kumar <rathiashwani8@gmail.com>
2023-07-30 22:22:53 +0530

    This is my second commit

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 app=Flask(__name__)
 
 @app.route('/')
+def welcome():
+    return "Welcome to the calculator page"
+
+@app.route('/calculator')
 def calculator():
     return render_template('index.html')
 
